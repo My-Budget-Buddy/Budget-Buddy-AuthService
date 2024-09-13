bf0f32a4772f0074b4e219d03e32d0f3464fffde
d-swe <djkim.swe@gmail.com>
2024-09-13 09:06:48 -0400

    added tests for controllers, models, services

--- src/test/java/com/skillstorm/authservice/AuthServiceRunnerTests.java
+++ src/test/java/com/skillstorm/authservice/AuthServiceRunnerTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AuthServiceRunnerTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
